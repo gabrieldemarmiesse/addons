6f41c6d91a3fefcff62e6af7631600c6c5c5d7f3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-04-17 10:29:50 +0000

    Try buildx.

--- .github/workflows/make_wheel_Linux.sh
+++ .github/workflows/make_wheel_Linux.sh
@@ -5,11 +5,12 @@ docker info
 # to get more disk space
 rm -rf /usr/share/dotnet &
 
-DOCKER_BUILDKIT=1 docker build \
+docker buildx build \
     -f tools/docker/build_wheel.Dockerfile \
     --output type=local,dest=wheelhouse \
     --build-arg PY_VERSION \
     --build-arg TF_VERSION \
     --build-arg NIGHTLY_FLAG \
     --build-arg NIGHTLY_TIME \
+    --cache-to=type=registry,ref=gabrieldemarmiesse/cache_for_addons \
     ./
